67d2d9e94c260257427e79d26ce061836ad96f16
Pigioty <Pigiotyreal@gmail.com>
2023-03-10 16:29:41 -0500

    Signup frontend

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
 app.listen(port, () => {
     console.log(`Now listening on port ${port}!`)
 })
