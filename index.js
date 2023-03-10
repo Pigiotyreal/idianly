480257bf8539d702da1353b55bc9e0bd9a1fb394
Pigioty <Pigiotyreal@gmail.com>
2023-03-10 16:37:37 -0500

    Connected to DB

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express")
+const sqlite3 = require("sqlite3").verbose()
 const app = express()
 const port = 3000
 
 app.set("view engine", "ejs")
+
+const db = new sqlite3.Database("./main.db", (err) => {
+    if(err) {
+        console.error(err.message)
+    }
+    console.log("Connected to the database!")
+})
+
+process.on("SIGINT", () => {
+    db.close((err) => {
+        if(err) {
+            console.error(err.message)
+        }
+        console.log("Closed the database connection")
+        process.exit(0)
+    })
+})
 
 app.get("/", (req, res) => {
     res.render("index")
